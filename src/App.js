751ee835514ac99ff79abc08abad98ef02165a87
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-09 17:34:31 -0500

    adding browser router for links to pages, adding comps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import About from "./components/Pages/About";
+import Portfolio from "./components/Pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Footer />
+      </div>
+    </Switch>
+    </Router>
+
+  )
 }
 
 export default App;
