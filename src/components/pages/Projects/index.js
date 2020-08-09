0f0e920be66eeb8de6f2fcdeb3cd10afe0945858
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-09 17:48:05 -0500

    Projects page, adding array to map over projects

---
+++ src/components/pages/Projects/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Project from "../../Project";
+import burger from "./burger.png";
+import cocktail from "./cocktail.jpg";
+import lily from "./lily.jpg";
+import empDir from "./employee-summary.jpg";
+import empManage from "./employee-tracker.jpg";
+import note from "./notetaker.jpg";
+import fitness from "./fitness-tracker.jpg";
+
+const portfolioData = [
+    {
+        id: 1,
+        title: "Cocktail Finder",
+        picture: cocktail,
+        repo: "https://github.com/emcarlisle/Drinking-At-Home",
+        demo: "https://emcarlisle.github.io/Drinking-At-Home/"
+    },
+    {
+        id: 2,
+        title: "Burger App",
+        picture: burger,
+        repo: "https://github.com/emcarlisle/burger",
+        demo: "https://burgerapphw11.herokuapp.com/"
+    },
+    {
+        id: 3,
+        title: "Employee Directory",
+        picture: empDir,
+        repo: "https://github.com/emcarlisle/react-employee-dir",
+        demo: "https://react-employe-dir.herokuapp.com/"
+    },
+    {
+        id: 4,
+        title: "Note Taker",
+        picture: note,
+        repo: "https://github.com/emcarlisle/Note-Taker",
+        demo: "https://note-takerhw11.herokuapp.com"
+    },
+    {
+        id: 5,
+        title: "Fitness Tracker",
+        picture: fitness,
+        repo: "https://github.com/emcarlisle/fitness-tracker",
+        demo: "https://fitnesstracker-hw17.herokuapp.com/"
+    },
+    {
+        id: 6,
+        title: "Employee Management System",
+        picture: empManage,
+        repo: "https://github.com/emcarlisle/Employee-Tracker",
+        demo: "https://drive.google.com/file/d/1iy_bJ7BLYjP3238-98rdhiFvbIZcA4lY/view?usp=sharing"
+    }
+]
