c011fee2019a6e53cd4a70863a85ad123022f5e9
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-09 17:18:57 -0500

    completed footer comp

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,11 @@ function Footer() {
             <nav className="footer-links">
                 <a type="button" role="button" className="button"><i className="fa fa-github" onClick={() => window.open("https://github.com/emcarlisle")}></i></a>
                 <a type="button" role="button" className="button"><i className="fa fa-linkedin-square" onClick={() => window.open("https://www.linkedin.com/in/carlisle-emily02/")}></i></a>
-
             </nav>
+            <div className="footer-copyright py-3">Feel free to contact me here:
+                <p>emilydeluna96@gmail.com</p>
+            </div>
         </footer>
     )
 }
+export default Footer;
